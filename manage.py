b92bedb0508f878f0cc53eea3613ca2b90e5fa48
DANILOILAQUIZE <dilaquitze@gmail.com>
2025-08-15 21:57:54 -0500

    NUEVOS CAMBIOS

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_voto_ue_riobamba.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_voto_ue_angamarca.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
